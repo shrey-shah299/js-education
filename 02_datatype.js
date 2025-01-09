a3b5edd6a47cf162f0aa2fad95c2be02a924e13c
shrey-shah299 <shreyshah026@gmail.com>
2025-01-09 11:48:56 +0530

    Add files via upload

---
+++ 02_datatype.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello") //we are using nodejs not browser
+
+/*
+number ->2 to power 53
+bigint
+string=>""
+boolean =>true/false 
+null =>standalone value //object
+undefined //undefined
+symbol =>unque
+
+object
+*/
+let state= "hello"
+let num=12
+let car=null
+console.log(typeof state);
+console.table([typeof num,typeof  car,typeof undefined ]);
