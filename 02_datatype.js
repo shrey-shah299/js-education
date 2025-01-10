7e0239ee5810172e587e2c847a2ec043d128cf3d
shrey-shah299 <shreyshah026@gmail.com>
2025-01-10 10:55:16 +0530

    Add files via upload

--- 02_datatype.js
+++ 02_datatype.js
@@ -18,3 +18,24 @@ let num=12
 let car=null
 console.log(typeof state);
 console.table([typeof num,typeof  car,typeof undefined ]);
+// primitive -7 types :string ,Number ,Boolean,Null ,undefined ,Symbol ,BigInt 
+
+// reference type :Arrays ,Objects ,Functions (javascript is dynamically defined)
+const id =Symbol('123' )
+const anotherId =Symbol('123')
+
+console.log(id ===anotherId); //not same
+
+//object
+ 
+let myObj={
+    name :"shrey",
+    age :22,
+}
+
+const myFunction =function(){
+    console.log("Hello world");
+}
+//null ==>typeof value will give object.
+//return tye of non-primitive datatypes is object functions 
+console.log(typeof myObj);
---
+++ 05_comparison.js
@@ -0,0 +1,13 @@
+///console.log("2">1);
+//console.log("02">1);
+
+console.log(null >0);
+console.log(null==0);
+console.log(null >=0);
+
+//comparison operator and == work differently <= convert null to number
+//same in undefined false in every case.
+
+// === strict check (both datatype and value)
+
+console.log("2"===2);
