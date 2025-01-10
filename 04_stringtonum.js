eddbb5a7cec08495740ba9dff688f712248b4f39
shrey-shah299 <shreyshah026@gmail.com>
2025-01-10 10:24:47 +0530

    Add files via upload

---
+++ 04_stringtonum.js
@@ -0,0 +1,32 @@
+// operations
+
+let value =3
+let negvalue =-value
+//console.log(negvalue);
+
+// ** to the power 2**3 =8
+// % modulus
+// / division
+
+//console.log("1"+ 2 )
+//console.log("1"+ 2 +2 )
+//console.log(1+ "2")
+//console.log(1+2+ "2")
+
+//if string is first all are converted to string and viceversa
+
+console.log(+true);
+console.log(+"");
+let num1 ,num2 ,num3
+num1 =num2 =num3 =4
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
